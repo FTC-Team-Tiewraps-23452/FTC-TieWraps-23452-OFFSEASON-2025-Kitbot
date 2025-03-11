184b3b2eb2aadb11db95d4b5c512da00874beda0
Medewerker <vksebastiaan@hotmail.com>
2025-03-11 15:20:40 +0100

    gewerkt aan de drivetrain niet getest.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystem/TankDrivetrain.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.robot.subsystem;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
+
+public class TankDrivetrain {
+
+private final DcMotor rightDrive;
+private final DcMotor leftDrive;
+    /**
+     * This is the constructor of the subsystem
+     * This is the function that will be run when the subsystem is created,
+     * which happens at the beginning of an OpMode.
+     * The constructor should have the same name as the class (ExampleMecanumDrivetrain in this case).
+     *
+     * @param hardwareMap This is the input of the constructor, which will be used
+     *                    to link the motors and servos in the code to the motors and servos
+     *                    on the actual robot
+     */
+    public TankDrivetrain(HardwareMap hardwareMap){
+        rightDrive = hardwareMap.get(DcMotor.class, "motor0");
+        leftDrive = hardwareMap.get(DcMotor.class, "motor1");
+
+        leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+    }
+
+    public void tankDrive(double x, double y, double rx){
+        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        double drive = -gamepad1.left_stick_y;
+        double turn  =  -gamepad1.right_stick_x;
+        double leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+        double rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
+        leftDrive.setPower(leftPower);
+        rightDrive.setPower(rightPower);
+
+    }
+
+}
